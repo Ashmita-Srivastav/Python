566c73e512882334b89935e07d15cd99b4dfec63
Ashmita-Srivastav <138620566+Ashmita-Srivastav@users.noreply.github.com>
2023-07-25 21:11:02 +0530

    Add files via upload

---
+++ Sum of squares of the first 100 natural numbers..py
@@ -0,0 +1,8 @@
+# To find the sum of squares of the first 100 natural numbers.
+
+n=100
+S=(n*(n+1)*(2*n+1))/6
+print("The formula to find sum of square of n natural number:(n*(n+1)*(2*n+1))/6 ")
+print("the sum of squares of the first 100 natural numbers is: ",S)
+
+    
