d8a5eb29c0668034267dadd8186fa5d57cb51c74
Ashmita-Srivastav <138620566+Ashmita-Srivastav@users.noreply.github.com>
2023-08-01 23:43:11 +0530

    Add files via upload

---
+++ addition_of x and y.py
@@ -0,0 +1,12 @@
+#addition of x and y
+
+
+#x = 30
+#y = 10
+x = int(input("enter the x value:"))
+y = float(input("enter the y value:"))
+if(x>0 or y>0): 
+     z = x + y
+     print("the value of x:",x,"the value of y:",y,"addition of x and y is:\n",z)
+else:
+    print("input value of x is less than zero")
