de610110d0a5c33e66fbabc207f07a1e9d5af9f3
Ashmita-Srivastav <138620566+Ashmita-Srivastav@users.noreply.github.com>
2023-08-09 23:39:52 +0530

    Add files via upload

---
+++ Tables.py
@@ -0,0 +1,22 @@
+A = int(input("Enter the number:"))
+print("THE TABLE OF",A,"IS") 
+B = A*1
+print(B)
+C = A*2
+print(C)
+D = A*3
+print(D)
+E = A*4
+print(E)
+F = A*5
+print(F)
+G = A*6
+print(G)
+H= A*7
+print(H)
+I = A*8
+print(I)
+J = A*9
+print(J)
+K = A*10
+print(K)
